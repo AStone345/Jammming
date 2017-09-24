a1c26232137eaf536b45e5c491bcf2cb30e942d4
Astone345 <astone345@gmail.com>
2017-09-24 16:38:42 -0500

    add/remove track functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import SearchBar from '../SearchBar';
-import SearchResults from '../SearchResults';
-import Playlist from '../Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchResults: [], 
+                  playlistName: 'any string',
+                  playlistTracks: []};
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks.push(track);
+
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({playlistTracks: tracks});
+  }
+
   render() {
     return (
       <div>
-  		<h1>Ja<span className="highlight">mmm</span>ing</h1>
-  		<div class="App">
-    		<!-- Add a SearchBar component -->
-    		<div className="App-playlist">
-      			<!-- Add a SearchResults component -->
-      			<!-- Add a Playlist component -->
-    		</div>
-  		</div>
-	</div>
+  		  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  		  <div className="App">
+    		  <SearchBar />
+    		  <div className="App-playlist">
+      			<SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+      			<Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+    		  </div>
+  		  </div>
+      </div>
     );
   }
 }
